86314d5d0c29ab3037b9e8215fb0679cd29e2d12
christinajoslin <129335539+christinajoslin@users.noreply.github.com>
2023-04-14 11:44:50 -0400

    Added a new class for reading/writing user information -Christina

---
+++ src/UserInfo.java
@@ -0,0 +1,9 @@
+/*********
+ * This class reads previous user information and writes current user information into buyer.csv and seller.csv
+ * @author Christina Joslin, lab sec 4427
+ * @version 4/14/2023
+ */
+public class UserInfo {
+
+    //TODO
+}
