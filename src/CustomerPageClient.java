03f20be90c10445bbbbe20d330a9524147f0a898
svenithalor <sthalor@purdue.edu>
2023-04-19 15:12:18 -0400

    switch cases for CustomerPageClient and CustomerPageServer

--- src/CustomerPageClient.java
+++ src/CustomerPageClient.java
@@ -12,28 +12,104 @@ public class CustomerPageClient {
         String host = scanner.nextLine();
         System.out.println("Enter a port number");
         int port = scanner.nextInt();
+        scanner.nextLine();
         try {
             Socket socket = new Socket(host, port);
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter writer = new PrintWriter(socket.getOutputStream());
             System.out.println("Connection established!");
-            int repeat = 0;
+            int repeat = 1;
             do {
                 System.out.println("Select an option: ");
                 System.out.println("1. View all available bikes");
-                    System.out.println("2. Sort bikes by quantity available");
-                    System.out.println("3. Sort bikes by price");
-                    System.out.println("4. Add a bike to cart");
-                    System.out.println("7. Search products");
                 System.out.println("2. Review cart");
-                    System.out.println("6. Edit cart");
-                    System.out.println(". Checkout");
                 System.out.println("3. Get purchase history");
                 System.out.println("4. Logout");
                 System.out.println("5. Delete account");
                 int choice = scanner.nextInt();
+                scanner.nextLine();
                 writer.println(choice);
-                //TODO
+                writer.flush();
+                switch (choice) {
+                    case 1: String bikeInfo = reader.readLine(); // case 1: display bikes
+                        while (bikeInfo != null) {
+                            System.out.println(bikeInfo);
+                            bikeInfo = reader.readLine();
+                        }
+                        System.out.println("1. Sort bikes by quantity available");
+                        System.out.println("2. Sort bikes by price");
+                        System.out.println("3. View bike");
+                        System.out.println("4. Go back");
+                        System.out.println("5. Search products");
+                        int choice1 = scanner.nextInt();
+                        scanner.nextLine();
+                        writer.println(choice1);
+                        writer.flush();
+                        switch (choice1) {
+                            case 1, 2: String sortedBikeInfo = reader.readLine();
+                                while (sortedBikeInfo != null) {
+                                    System.out.println(sortedBikeInfo);
+                                    sortedBikeInfo = reader.readLine();
+                                }
+                                break;
+                            case 3:
+                                System.out.println("Enter id of bike to view");
+                                int id = scanner.nextInt();
+                                scanner.nextLine();
+                                writer.println(id);
+                                System.out.println(reader.readLine());
+                                System.out.println("1. Add to cart");
+                                System.out.println("2. Go back");
+                                int cart = Integer.parseInt(scanner.nextLine());
+                                if (cart == 1) {
+                                    // TODO: add to cart implementation
+                                }
+                                break;
+                            case 4: break;
+                            case 5:
+                                System.out.println("Enter search"); // TODO: search box text field
+                                String search = scanner.nextLine();
+                                writer.println(search);
+                                writer.flush();
+                                String result = reader.readLine();
+                                while (result != null) {
+                                    System.out.println(result);
+                                    result = reader.readLine();
+                                }
+                                break;
+                        }
+                        break;
+                    case 2: // TODO: view/edit cart and checkout
+                        break;
+                    case 3: System.out.println("Enter name of file to export data to");
+                        String fileName = scanner.nextLine();
+                        writer.println(fileName);
+                        writer.flush();
+                        String success = reader.readLine();
+                        if (success.equals("true")) {
+                            System.out.println("Success!");
+                        } else if (success.equals("false")) {
+                            System.out.println("An error occurred, try again!");
+                        }
+                        break;
+                    case 4:
+                        repeat = 0;
+                        break; // TODO: implement logout
+                    case 5:
+                        System.out.println("Enter username to confirm account deletion or enter 1 to cancel");
+                        String confirm = scanner.nextLine();
+                        writer.println(confirm);
+                        String deleted = reader.readLine();
+                        if (confirm.equals("1")){
+                            break;
+                        } else if (deleted.equals("true")) {
+                            System.out.println("Account deleted successfully!");
+                            repeat = 0;
+                        } else if (deleted.equals("false")) {
+                            System.out.println("Try again!");
+                        }
+                        break;
+                }
             } while (repeat == 1);
         } catch(UnknownHostException uhe) {
 
--- src/CustomerPageServer.java
+++ src/CustomerPageServer.java
@@ -1,3 +1,185 @@
+import java.io.File;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
 public class CustomerPageServer {
-    //TODO
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(1234);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter writer = new PrintWriter(socket.getOutputStream());
+                int repeat = 1;
+                while (repeat == 1) {
+                    int choice = Integer.parseInt(reader.readLine());
+                    ArrayList<Bike> bikes = UserInfo.getBikes();
+                    switch(choice) {
+                        case 1:
+                            for (Bike bike : bikes) {
+                                writer.println(bike.toString());
+                                writer.flush();
+                            }
+                            int choice1 = Integer.parseInt(reader.readLine());
+                            switch (choice1) {
+                                case 1: ArrayList<Bike> quantitySorted = sortByQuantity(bikes);
+                                    for (Bike bike: quantitySorted) {
+                                        writer.println(bike.toString());
+                                        writer.flush();
+                                    }
+                                    break;
+                                case 2: ArrayList<Bike> priceSorted = sortByPrice(bikes);
+                                    for (Bike bike: priceSorted) {
+                                        writer.println(bike.toString());
+                                        writer.flush();
+                                    }
+                                    break;
+                                case 3: int id = Integer.parseInt(reader.readLine());
+                                    Bike bikeToView = null;
+                                    for (Bike bike : bikes) {
+                                        if (bike.getId() == id) {
+                                            bikeToView = bike;
+                                            break; // breaks out of for loop not switch case
+                                        }
+                                    }
+                                    if (bikeToView == null) {
+                                        writer.println("No bike found!");
+                                        writer.flush();
+                                    } else {
+                                        writer.println(bikeToView.toString());
+                                        writer.flush();
+                                    }
+                                    break;
+                                case 4: break;
+                                case 5: String searchTerm = reader.readLine();
+                                    ArrayList<Bike> matches = search(searchTerm, bikes);
+                                    if (matches != null) {
+                                        for (Bike bike : matches) {
+                                            writer.println(bike.toString());
+                                            writer.flush();
+                                        }
+                                    } else {
+                                        writer.println("No matches!");
+                                        writer.flush();
+                                    }
+                                    break;
+                            }
+                            break;
+                        case 2: break; // TODO: shopping cart implementation
+                        case 3: String fileName = reader.readLine();
+                            writer.println(getPurchaseHistory(fileName));
+                            writer.flush();
+                            break;
+                        case 5: String user = reader.readLine();
+                            boolean deleted = deleteAccount(user);
+                            writer.println(deleted);
+                            writer.flush();
+                            break;
+                    }
+                }
+            }
+        } catch (IOException ioe) {
+
+        }
+    }
+
+    public static ArrayList<Bike> sortByPrice(ArrayList<Bike> bikes) {
+        ArrayList<Bike> sorted = new ArrayList(bikes.size());
+        for (int i = 0; i < bikes.size(); i++) {
+            sorted.add(bikes.get(i));
+        }
+        for (int i = 0; i < bikes.size() - 1; i++) {
+            Bike thisBike = sorted.get(i);
+            Bike lowestBike = thisBike;
+            int lowestIndex = i;
+            double lowestPrice = lowestBike.getPrice();
+            for (int j = i + 1; j < bikes.size(); j++) {
+                Bike otherBike = sorted.get(j);
+                double price = otherBike.getPrice();
+                if (price < lowestPrice) {
+                    lowestBike = otherBike;
+                    lowestPrice = price;
+                    lowestIndex = j;
+                }
+            }
+            sorted.set(i, lowestBike);
+            sorted.set(lowestIndex, thisBike);
+        }
+        return sorted;
+    }
+
+    public static ArrayList<Bike> sortByQuantity(ArrayList<Bike> bikes) {
+        ArrayList<Bike> sorted = new ArrayList(bikes.size());
+        for (int i = 0; i < bikes.size(); i++) {
+            sorted.add(bikes.get(i));
+        }
+        for (int i = 0; i < bikes.size() - 1; i++) {
+            Bike thisBike = sorted.get(i);
+            Bike lowestBike = thisBike;
+            int lowestIndex = i;
+            double lowestQuantity = lowestBike.getQuantity();
+            for (int j = i + 1; j < bikes.size(); j++) {
+                Bike otherBike = sorted.get(j);
+                double quantity = otherBike.getQuantity();
+                if (quantity < lowestQuantity) {
+                    lowestBike = otherBike;
+                    lowestQuantity = quantity;
+                    lowestIndex = j;
+                }
+            }
+            sorted.set(i, lowestBike);
+            sorted.set(lowestIndex, thisBike);
+        }
+
+        return sorted;
+    }
+
+    public static ArrayList<Bike> search(String searchTerm, ArrayList<Bike> bikes) {
+        ArrayList<Bike> matches = new ArrayList<Bike>();
+        String term = searchTerm.toLowerCase();
+        for (Bike bike : bikes) {
+            if (bike.getModelName().contains(term)) {
+                matches.add(bike);
+            } else if (bike.getSellerName().contains(term)) {
+                matches.add(bike);
+            } else if (bike.getDescription().contains(term)) {
+                matches.add(bike);
+            }
+        }
+        if (matches.size() == 0) {
+            return null;
+        } else {
+            return matches;
+        }
+    }
+
+    public static boolean getPurchaseHistory(String fileName) {
+        try {
+            File file = new File(fileName);
+            PrintWriter pw = new PrintWriter(file);
+            for (PurchasedBike b : purchasedBikes) {
+                pw.println(b.toString());
+            }
+            return true;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public static boolean deleteAccount(String user) {
+        boolean deleted = false;
+        ArrayList<Buyer> buyers = UserInfo.getBuyers();
+        for (Buyer buyer : buyers) {
+            if (buyer.getUsername().equalsIgnoreCase(user)) {
+                buyers.remove(buyer);
+                UserInfo.setBuyers(buyers);
+                deleted = true;
+            }
+        }
+        return deleted;
+    }
 }
