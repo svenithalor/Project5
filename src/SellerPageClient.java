7c42d718d3b66abb334d748db86ec848554aea05
AB11091 <114780264+AB11091@users.noreply.github.com>
2023-04-22 20:49:53 +0000

    working on client-server communication

--- src/SellerPageClient.java
+++ src/SellerPageClient.java
@@ -4,17 +4,73 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class SellerPageClient {
-    //TODO
+    // fields
+    private String name;
+    private ArrayList<Bike> inventory;
+
+
+    public static Bike parseBike(String newBike) {
+        
+        String[] sNB = newBike.split(",");
+        int wS = Integer.valueOf(sNB[1]);
+        double p = Double.valueOf(sNB[2]);
+        boolean u = Boolean.parseBoolean(sNB[4]);
+        int q = Integer.valueOf(sNB[7]);
+        int tempID = Integer.valueOf(sNB[8]);
+
+        Bike bike = new Bike(sNB[0], wS, p, sNB[3], u, sNB[5], sNB[6], q, tempID);
+    }
+
+    /******
+     * Method to send arrayList from client to server or vice versa.
+     */
+    public static String sendArrayList(ArrayList<Bike> bike) {
+        StringBuilder sb = new StringBuilder();
+        for (Bike b : bikes) {
+            sb.append(b.toString() + "]");
+        }
+
+        if (sb.length() != 0) {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        String vtr = sb.toString();
+        return vtr;
+    }
+
+    /********
+     * Method to recieve arrayList from other side
+     */
+    public ArrayList<Bike> recieveArrayList(String s) {
+        String[] bikes = s.split(']');
+        ArrayList<Bike> vtr = new ArrayList<>();
+
+        for (String bike : bikes) {
+            vtr.add(parseBike(bike));
+        }
+
+        return vtr;
+    }
+
+    // The above 3 methods are the same as the SellerPageServer.
+    // Both classes will need them because they are sending and recieving data back and forth.
     public SellerPageClient(String name, ArrayList<Bike> inventory) {
         this.name = name;
         this.inventory = inventory;
     }
+
+    
+    
     public void runSellerPageClient(Seller seller) {
         try {
             SellerPageClient C = new SellerPageClient(seller.getUsername(), seller.getInventory()); //creates an object to be used to navigate the menu
             Socket socket = new Socket("localhost", 4242);
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+            writer.write(seller.getUsername());
+            writer.println();
+            writer.flush(); // sends the seller name
             do {
                 int o = C.displayMainMenu(writer, reader); // writer and reader have already been created
 
--- src/SellerPageServer.java
+++ src/SellerPageServer.java
@@ -4,6 +4,73 @@ import javax.swing.*;
 import java.util.ArrayList;
 import java.lang.StringBuilder;
 public class SellerPageServer {
+
+    // fields
+    private String name;
+    private ArrayList<Bike> inventory;
+
+    public SellerPageServer(String name, ArrayList<Bike> bikes) {
+        this.name = name;
+        this.inventory = bikes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Bike> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ArrayList<Bike> i) {
+        this.inventory = i;
+    }
+
+    /*******
+     * Creates a Bike object when passed in the toString() format
+     */
+    public static Bike parseBike(String newBike) {
+        
+        String[] sNB = newBike.split(",");
+        int wS = Integer.valueOf(sNB[1]);
+        double p = Double.valueOf(sNB[2]);
+        boolean u = Boolean.parseBoolean(sNB[4]);
+        int q = Integer.valueOf(sNB[7]);
+        int tempID = Integer.valueOf(sNB[8]);
+
+        Bike bike = new Bike(sNB[0], wS, p, sNB[3], u, sNB[5], sNB[6], q, tempID);
+    }
+
+    /******
+     * Method to send arrayList from client to server or vice versa.
+     */
+    public static String sendArrayList(ArrayList<Bike> bike) {
+        StringBuilder sb = new StringBuilder();
+        for (Bike b : bikes) {
+            sb.append(b.toString() + "]");
+        }
+
+        if (sb.length() != 0) {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        String vtr = sb.toString();
+        return vtr;
+    }
+
+    /********
+     * Method to recieve arrayList from other side
+     */
+    public ArrayList<Bike> recieveArrayList(String s) {
+        String[] bikes = s.split(']');
+        ArrayList<Bike> vtr = new ArrayList<>();
+
+        for (String bike : bikes) {
+            vtr.add(parseBike(bike));
+        }
+
+        return vtr;
+    }
     try {
         ServerSocket ss = new ServerSocket(4242); // change this port later
 
@@ -11,6 +78,18 @@ public class SellerPageServer {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter writer = new PrintWriter(socket.getOutputStream()); 
+
+        String sellerName = reader.readLine(); // reads seller name first
+
+        String sellerInventory = reader.readLine(); // reads the inventory immediately after
+
+        ArrayList<Bike> inv = sellerInventory.recieveArrayList();
+
+        Seller seller = new Seller(sellerName, inv);
+
+        
+
+
         String strOption = null;
         int option = null;
         do {
